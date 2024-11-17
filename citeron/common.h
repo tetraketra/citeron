0c2c02f6a5eb0aba4496e58b642d20b4a08d3067
Tetra Silverberg <tetra.silv@gmail.com>
2024-11-17 00:51:15 -0500

    Undef then redef `assert` to avoid name conflict.

--- citeron/common.h
+++ citeron/common.h
@@ -4,13 +4,17 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <assert.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <string.h>
 
 #define ignore(any)   ((void)(any))
 
 #define countof(arr)  (size_t)(sizeof(arr) / sizeof(*(arr)))
 #define lengthof(arr) (countof(arr) - 1)
 
-#define assert(cond)  while (!(cond)) { __builtin_unreachable() }
+#undef  assert
+#define assert(cond)  while (!(cond)) { __builtin_unreachable(); }
 /*     `static_assert(cond)` (lowercase) is available via `assert.h` as of C23. */
 
 #endif
